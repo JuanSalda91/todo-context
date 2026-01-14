e2f8dc349f5f483825946debaf284e78d0ab0583
JuanSalda91 <juansalda9131@gmail.com>
2026-01-14 00:07:37 -0500

    context file AppProviders completed, file were all components and contexts are connected

---
+++ src/context/AppProviders.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from 'react';
+import { TodoProvider } from './TodoContext';
+import { FilterProvider } from './FilterContext';
+import { ThemeProvider } from './ThemeContext';
+
+interface AppProvidersProps {
+  children: ReactNode;
+}
+
+export function AppProviders({ children }: AppProvidersProps) {
+  return (
+    <ThemeProvider>
+      <TodoProvider>
+        <FilterProvider>
+          {children}
+        </FilterProvider>
+      </TodoProvider>
+    </ThemeProvider>
+  );
+}
